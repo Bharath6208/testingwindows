be9406b6a882ef3d2ea2e46de8cdc4dd4043df02
Bharath Kumar <bmopuri@progress.com>
2022-09-20 18:11:18 +0530

    config files for bkm_ec2

---
+++ config.rb
@@ -0,0 +1,6 @@
+client_name     = 'testwin'
+client_key      = '/Users/bmopuri/Documents/test/chef-repo/testwin.pem'
+chef_server_url = 'https://chef-server-ec2/organizations/pinkypie'
+
+#knife[:vault_mode] = 'client'
+#knife[:vault_admins] = 'che_user_2'
